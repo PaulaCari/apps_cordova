a2bb7115f2cb90e996c3079e9178b719fcda9419
Paula Cari <paulola0223@gmail.com>
2024-09-25 08:47:11 -0300

    adicione cada item nos card img,nome,rating,preço e conversão de preço

--- www/js/index.js
+++ www/js/index.js
@@ -9,9 +9,40 @@ fetch('js/backend.json')
     // utilizar localstorage(armazenamento local)
     localStorage.setItem('produtos', JSON.stringify(data));
     console.log('Dados dos produtos salvos no localStorage');
+
+    // esvaziar a area de produtos (escodeu os card)
+    $("#produtos").empty();
+
+    // percorrido de  cada items
+    data.forEach(produto =>{
+        var produtoHTML = `
+             <!-- item card -->
+                <div class="item-card">
+                  <a data-id="${produto.id}" href="#" class="item">
+                    <div class="img-container">
+                      <img  src="${produto.imagem}">
+                    </div>
+
+                    <div class="nome-rating">
+                      <span class="color-gray">${produto.nome}</span>
+                      <span class="bold margin-right">
+                        <i class="mdi mdi-star"></i>
+                       ${produto.rating}
+                      </span>
+                    </div>
+                    <div class="price"> ${produto.preco_promocional.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</div>
+                  </a>
+                </div>
+        `;
+        // adiciona os produtos nos card
+        $("#produtos").append(produtoHTML)
+        
+    });
+
 })
 // se der errado
 .catch(error => console.error('erro ao fazer fech dos dados: '+error));
 
 
 
+// alimentar a area dos produtos(card)
