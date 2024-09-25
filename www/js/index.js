26e164bfb4d7919b5bf0d581e7c511bec88232aa
Paula Cari <paulola0223@gmail.com>
2024-09-25 08:13:50 -0300

    adicione requisição para o backend

--- www/js/index.js
+++ www/js/index.js
@@ -1,2 +1,17 @@
 /*teste comando*/
 /*app.dialog.alert('ola mundo');*/
+
+//requisição protocolo http, para backend
+fetch('js/backend.json')
+.then(Response => Response.json())
+.then(data=>{
+    // salvar os dados vindos do back-end localmente
+    // utilizar localstorage(armazenamento local)
+    localStorage.setItem('produtos', JSON.stringify(data));
+    console.log('Dados dos produtos salvos no localStorage');
+})
+// se der errado
+.catch(error => console.error('erro ao fazer fech dos dados: '+error));
+
+
+
