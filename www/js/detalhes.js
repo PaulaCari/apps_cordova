a3813deff926f58704c7cfd42ea79bf41b21705f
Paula Cari <paulola0223@gmail.com>
2024-09-25 10:00:58 -0300

    criar os id emcada item e Alimentar os valores dos item

--- www/js/detalhes.js
+++ www/js/detalhes.js
@@ -9,7 +9,13 @@ var item = produtos.find(produto => produto.id === id);
 if (item){
     console.log('Produto encontrado: ',item);
 
-    
+    // Alimentar os valores dos item 
+    $("#imagem-detalhe").attr('src', item.imagem)
+    $("#nome-detalhe").html(item.nome);
+    $("#rating-detalhe").html(item.rating)
+    $("#like-detalhe").html(item.likes)
+    $("#reviews-detalhe").html(item.reviews + ' reviews');
+    $("#descricao-detalhe").html(item.descricao)
 
 
 
