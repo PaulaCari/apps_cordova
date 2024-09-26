ea6722ab7710ffd9e8aa56ced8656796a56704bf
Paula Cari <paulola0223@gmail.com>
2024-09-26 09:16:55 -0300

    Responsavel pela logica da pag de carrinho

--- www/js/carrinho.js
+++ www/js/carrinho.js
@@ -1,4 +1,21 @@
 // teste de comunicação
-app.dialog.alert('disparou');
+//app.dialog.alert('disparou');
 
+// Responsavel pela logica da pag de carrinho
+var localCarrinho = localStorage.getItem('carrinho');
+
+if(localCarrinho){
+    var carrinho = JSON.parse(localCarrinho);
+    if(carrinho.length >0){
+        // tem item no carrinho
+        // Renderizar o carrinho
+        // somar totais dos produtos
+    }else{
+        //mostrar carrinho vazio
+
+    }
+
+}else{
+    // Mostrar carrinho vazio
+}
 
